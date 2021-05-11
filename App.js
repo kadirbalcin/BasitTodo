d25394b37c2ff0a921c1c80d095a0a7f7235fc42
Kadir Balçın <kadirbalcin145@gmail.com>
2021-05-11 05:26:34 +0300

    ScroolView Eklenmesi

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,ScrollView } from 'react-native';
+import ItemList from './ItemList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text:'',
+      data:[]
+    }
+  }
+
+  handleSave = () => {
+    const { text, data } = this.state;
+    data.push({text});
+    this.setState( {data, text:''} );
+  };
+
+  render() {
+    const { text, data } = this.state;
+    return (
+      <View style={[{flex:1, paddingTop:30}]}>
+        <View style={styles.title}><Text style={styles.title_text}>To-Do Application</Text></View>
+        <View style={{ backgroundColor: '#ddd', padding: 10, flexDirection: 'row' }}>
+          <TextInput
+            style={styles.input}
+            value={text}
+            onChangeText={(text)=>{this.setState({text})}}
+          />
+          <TouchableOpacity onPress={this.handleSave} style={styles.button}>
+            <Text style={styles.title_text}>Ekle</Text>
+          </TouchableOpacity>
+        </View>
+        <ScrollView>
+          <View>
+              { data.map((item) => {
+                  return <ItemList text={item.text} />
+                })
+              }
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  title: {
+    backgroundColor: 'red',
+    padding: 10
   },
+  title_text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '700'
+  },
+  input: {
+    padding: 10, 
+    backgroundColor: 'white',
+    flex: 1
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'red',
+    borderRadius: 5,
+    marginLeft: 10
+  }
 });
